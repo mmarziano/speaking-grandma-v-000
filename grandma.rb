3fb0787172f10254d59127089cb5cb82d0a24507
Monica Marziano <mmarziano1@gmail.com>
2018-03-30 00:13:14 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,6 +15,9 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase != "I LOVE YOU GRANDMA!"
+    return "HUH? SPEAK UP, SONNY!"
+  
   end
 
 
